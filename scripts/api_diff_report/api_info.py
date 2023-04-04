2af340fc050070d032840e78c7a032d7305d831e
Mou <sunmou@google.com>
2023-04-03 23:59:03 -0700

    test objc

--- scripts/api_diff_report/api_info.py
+++ scripts/api_diff_report/api_info.py
@@ -38,22 +38,24 @@ def main():
                 text=True, 
                 check=False)
       logging.info("------------")
-      logging.info(result.stdout)
+      api_info = result.stdout
+      logging.info(api_info)
       file_path = os.path.join(output_dir, os.path.basename(file_name) + ".json")
       logging.info(file_path)
       with open(file_path, 'w') as f:
-        f.write(result.stdout)
+        f.write(api_info)
     elif file_name.endswith('.h'):
       result = subprocess.Popen(f"sourcekitten doc --objc {file_name} -- -x objective-c -isysroot $(xcrun --show-sdk-path) -I $(pwd)", 
                                 universal_newlines=True, 
                                 shell=True, 
                                 stdout=subprocess.PIPE)
       logging.info("------------")
-      logging.info(result.stdout.read())
+      api_info = result.stdout.read()
+      logging.info(api_info)
       file_path = os.path.join(output_dir, os.path.basename(file_name) + ".json")
       logging.info(file_path)
       with open(file_path, 'w') as f:
-        f.write(str(result.stdout.read(), 'UTF-8'))
+        f.write(api_info)
 
 
 
