fb5468bd7478fb103b90cb4feae3eac52f9d6ab8
Mou <sunmou@google.com>
2023-04-11 13:25:55 -0700

    build target

--- scripts/api_diff_report/api_info.py
+++ scripts/api_diff_report/api_info.py
@@ -67,11 +67,11 @@ def main():
 
       match = re.search(fr"Firebase(.*?){os.sep}", file_path)
       if match:
-        target = f"Firebase{match.groups()[0]}Unit"
+        scheme = f"Firebase{match.groups()[0]}"
         file_name = os.path.splitext(os.path.basename(file_path))[0]
-        if target not in swift_to_objc:
-          swift_to_objc[target] = []
-        swift_to_objc[target].append(file_name)
+        if scheme not in swift_to_objc:
+          swift_to_objc[scheme] = []
+        swift_to_objc[scheme].append(f"{file_name}-Swift.h")
       else:
         logging.error(f"no target matching file: {file_path}")
     elif file_path.endswith('.h') and "Public" in file_path:
@@ -86,7 +86,7 @@ def main():
       with open(output_path, 'w') as f:
         f.write(api_info)
 
-  for target, files in swift_to_objc.items():
+  for scheme, files in swift_to_objc.items():
       result = subprocess.Popen("scripts/setup_spm_tests.sh", 
                                 universal_newlines=True, 
                                 shell=True, 
@@ -94,16 +94,29 @@ def main():
       logging.info("------------")
       build_info = result.stdout.read()
       logging.info(build_info)
-      result = subprocess.Popen(f"xcodebuild -scheme {target} -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' ONLY_ACTIVE_ARCH=YES CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=YES COMPILER_INDEX_STORE_ENABLE=NO CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++ IPHONEOS_DEPLOYMENT_TARGET=13.0 TVOS_DEPLOYMENT_TARGET=13.0 BUILD_DIR={output_dir}", 
+      result = subprocess.Popen(f"xcodebuild -scheme {scheme} -sdk iphonesimulator ONLY_ACTIVE_ARCH=YES CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=YES COMPILER_INDEX_STORE_ENABLE=NO CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++ IPHONEOS_DEPLOYMENT_TARGET=13.0 TVOS_DEPLOYMENT_TARGET=13.0 BUILD_DIR={output_dir}", 
                                 universal_newlines=True, 
                                 shell=True, 
                                 stdout=subprocess.PIPE)
       logging.info("------------")
       build_info = result.stdout.read()
       logging.info(build_info)
-    
 
-
+      for file_dir, _, file_names in os.walk(output_dir):
+        for file_name in file_names:
+          if file_name in files:
+            logging.info(file_path)
+            file_path = os.path.join(file_dir, file_name)
+            result = subprocess.Popen(f"sourcekitten doc --objc {file_path} -- -x objective-c -isysroot $(xcrun --show-sdk-path) -I $(pwd)", 
+                                      universal_newlines=True, 
+                                      shell=True, 
+                                      stdout=subprocess.PIPE)
+            logging.info("------------")
+            api_info = result.stdout.read()
+            output_path = os.path.join(output_dir, file_name + ".json")
+            logging.info(output_path)
+            with open(output_path, 'w') as f:
+              f.write(api_info)
 
 
 def parse_cmdline_args():
