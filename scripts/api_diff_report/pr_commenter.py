6f858390a4d2bb5b79e3ad16e411a537352f0b8a
Mou <sunmou@google.com>
2023-05-05 15:06:43 -0700

    fix issue

--- scripts/api_diff_report/pr_commenter.py
+++ scripts/api_diff_report/pr_commenter.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
@@ -27,9 +28,9 @@ from requests.packages.urllib3.util.retry import Retry
 STAGES_PROGRESS = "progress"
 STAGES_END = "end"
 
-TITLE_PROGESS = "## Detecing API diff in progress...\n"
+TITLE_PROGESS = "## ⏳&nbsp; Detecing API diff in progress...\n"
 TITLE_END_DIFF = '## Apple API Diff Report\n'
-TITLE_END_NO_DIFF = "## No API diff detected\n"
+TITLE_END_NO_DIFF = "## ✅&nbsp; No API diff detected\n"
 
 COMMENT_HIDDEN_IDENTIFIER = '\r\n<hidden value="api-diff-report-comment"></hidden>\r\n'
 GITHUB_API_URL = 'https://api.github.com/repos/sunmou99/firebase-ios-sdk'
@@ -114,7 +115,7 @@ def get_comment_id(token, issue_number, comment_identifier):
 
 
 def list_comments(token, issue_number):
-  """https://docs.github.com/en/rest/reference/issues#list-issue-comments"""
+  # """https://docs.github.com/en/rest/reference/issues#list-issue-comments"""
   url = f'{GITHUB_API_URL}/issues/{issue_number}/comments'
   headers = {
       'Accept': 'application/vnd.github.v3+json',
